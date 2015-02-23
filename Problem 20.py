2180df5181679784869de252e972f55f3fcd94a4
xinlutu <tujieyi@gmail.com>
2015-02-23 13:17:41 -0600

    Problem 20

---
+++ Problem 20.py
@@ -0,0 +1,15 @@
+def factorial (n):
+    
+    if (n==1): return 1
+    
+    else:
+        
+        return n * factorial (n-1)
+
+sum = 0
+
+for i in str(factorial (100)):
+    
+    sum += int(1)
+
+print(sum)
